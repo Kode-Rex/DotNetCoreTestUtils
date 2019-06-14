a75c8d8c4f9ad6f87e36f7b70b800e903182540b
T-rav <t@stoneagetechnologies.com>
2019-06-14 14:35:31 -0700

    Added MockOptions and MockOptionsSnapshot for testing IoC build Options objects

---
+++ TestUtils/MockOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Options;
+
+namespace StoneAge.TestUtils
+{
+    public class MockOptions<T> : IOptions<T> where T: class, new()
+    {
+        public T Value { get; }
+
+        public MockOptions(T value)
+        {
+            Value = value;
+        }
+    }
+
+}
---
+++ TestUtils/MockOptionsSnapshot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Options;
+
+namespace StoneAge.TestUtils
+{
+    public class MockOptionsSnapshot<T> : IOptionsSnapshot<T> where T: class, new()
+    {
+        public T Value { get; }
+
+        public MockOptionsSnapshot(T value)
+        {
+            Value = value;
+        }
+
+        public T Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
