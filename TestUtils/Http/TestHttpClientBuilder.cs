d77549da2325cdcd1d5804e41c635661d1fd5918
Trav <t@koderex.co.za>
2020-09-25 14:08:26 -0600

    feat: added TestHttpClientBuilder

---
+++ TestUtils/Http/TestHttpClientBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace StoneAge.TestUtils
+{
+    public class TestHttpClientBuilder
+    {
+        private string _payload;
+
+        public TestHttpClientBuilder()
+        {
+            _payload = "{}";
+        }
+
+        public TestHttpClientBuilder With_Payload(string payload)
+        {
+            _payload = payload;
+            return this;
+        }
+
+        public HttpClient Create()
+        {
+            var handler = new TestHttpMessageHandler()
+                                .With_Payload(_payload);
+
+            return new HttpClient(handler);
+        }
+    }
+}
---
+++ TestUtils/Http/TestHttpMessageHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StoneAge.TestUtils
+{
+    internal class TestHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly List<string> _urlPayload;
+        private int _requestCount;
+
+        public HttpRequestMessage RequestMessage { get; set; }
+
+        internal TestHttpMessageHandler()
+        {
+            _urlPayload = new List<string>();
+            _requestCount = 0;
+        }
+
+        internal TestHttpMessageHandler With_Payload(string payload)
+        {
+            _urlPayload.Add(payload);
+            return this;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+                                                                     CancellationToken cancellationToken)
+        {
+            RequestMessage = request;
+
+            var payload = Set_Payload_For_Request();
+
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(payload)
+            };
+
+            _requestCount++;
+            return await Task.FromResult(responseMessage);
+        }
+
+        private string Set_Payload_For_Request()
+        {
+            var payload = "{}";
+            if (_requestCount < _urlPayload.Count)
+            {
+                payload = _urlPayload[_requestCount];
+            }
+
+            return payload;
+        }
+    }
+}
